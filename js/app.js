151c380de2f27b40fe74bc070d8e62339cf57327
Alex Gold <alex@alexgold.net>
2015-08-16 14:34:10 +1000

    comp number and user number

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 
 $(document).ready(function(){
 	
+	compNumber = Math.floor((Math.random() * 100) + 1);
+	console.log(compNumber); // verification
+
+	var userNumber = $("#userGuess").val();
+	console.log(userNumber + " is the user number");
+	
+	$("#guessButton").click(function(){
+		var userNumber = $("#userGuess").val();
+		event.preventDefault(); // prevent refresh for testing
+		console.log("clicked"); //verify
+		console.log(userNumber + " is the user number"); //verify
+	});
+
+
+
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
