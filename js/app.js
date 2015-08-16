4745fd1801075d1a649d5ddf63146d55b43e0e77
Alex Gold <alex@alexgold.net>
2015-08-16 20:51:37 +1000

    first draft

--- js/app.js
+++ js/app.js
@@ -4,38 +4,52 @@ $(document).ready(function(){
 	
 	
 	$("#guessButton").click(function(){
-		var compNumber = Math.floor((Math.random() * 100) + 1);
-		console.log(compNumber + " is the comp number"); // verification	
-		var userNumber = $("#userGuess").val();
+		console.log(compNumber + " is the comp number"); // verification
 		event.preventDefault(); // prevent refresh for testing
 		console.log("clicked"); //verify
 		console.log(userNumber + " is the user number"); //verify
 
+		var userNumber = parseInt($("#userGuess").val());
+
 		var compareNumber = Math.abs(compNumber - userNumber);
 		console.log (compareNumber + " is the difference");
 
-		if (0 < compareNumber && compareNumber < 10) {
-			console.log("HOT HOT HOT");
-		}
-		else if (10 <= compareNumber && compareNumber <= 25) {
-			console.log("HOT");
-		}
-		else if (25 < compareNumber && compareNumber <= 40) {
-			console.log("lukewarm");
-		}
-		else if (40 < compareNumber && compareNumber <= 70) {
-			console.log("cold");
-		}
-		else if (70 < compareNumber) {
-			console.log("ice cold");
-		}
-		else if (compareNumber == 0) {
-			console.log("YOU GOT IT!");
-		}
+		if (userNumber <= 100 && userNumber >= 0) {
+			
+			console.log("valid number"); //verify
 
+			if (0 < compareNumber && compareNumber < 10) {
+				$("#feedback").text("HOT HOT HOT");
+			}
+			else if (10 <= compareNumber && compareNumber <= 25) {
+				$("#feedback").text("HOT");
+			}
+			else if (25 < compareNumber && compareNumber <= 40) {
+				$("#feedback").text("lukewarm");
+			}
+			else if (40 < compareNumber && compareNumber <= 70) {
+				$("#feedback").text("cold");
+			}
+			else if (70 < compareNumber) {
+				$("#feedback").text("ice cold");
+			}
+			else if (compareNumber == 0) {
+				$("#feedback").text("YOU GOT IT!");
+			}
+
+			$("#guessList").append(userNumber + "  ");
+			$("#count").html(function(i, val) {
+				return +val+1
+			}); 
+		} else {
+			alert("Please input a number between 0 and 100!");
+		}
 	});
 
-
+	$(".new").click(function(){
+		console.log("new game clicked");
+		location.reload();
+	});
 
 
 	/*--- Display information modal box ---*/
@@ -50,5 +64,8 @@ $(document).ready(function(){
   	});
 
 });
+
+
+var compNumber = Math.floor((Math.random() * 100) + 1);
 
 
